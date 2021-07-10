751732f57810434ecf7fcce38dfa056efb347902
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-07-10 18:24:05 +0100

    Add comments

--- script.js
+++ script.js
@@ -55,7 +55,6 @@ class Cloud {
 
         if (typeof findUserIndex(email) === "number") {
             twoAlert("Account already exists.");
-
             return;
         }
 
@@ -87,17 +86,15 @@ class Cloud {
 
         if (saveUsers[index].files[nameFile]) {
             twoAlert("File already exists in the account.");
-
             return;
         }
 
         if (saveUsers[index].memory - size < 0) {
             twoAlert("File size exceeds account capacity.");
-
             return;
         }
-        saveUsers[index].files[nameFile] = size;
 
+        saveUsers[index].files[nameFile] = size;
         saveUsers[index].memory -= size;
 
         twoAlert("File uploaded into account.");
@@ -111,25 +108,21 @@ class Cloud {
 
         if (indexUser === undefined || indexShareUser === undefined) {
             twoAlert("Account does not exist.");
-
             return;
         }
 
         if (!saveUsers[indexUser].files[fileName]) {
             twoAlert("File does not exist.");
-
             return;
         }
 
         if (saveUsers[indexUser].type === "Basic") {
             twoAlert("Account does not allow file sharing.");
-
             return;
         }
 
         if (saveUsers[indexUser].filesShared[fileName]) {
             twoAlert("File already shared.");
-
             return;
         }
 
@@ -139,10 +132,10 @@ class Cloud {
                 saveUsers[indexUser].files[fileName] / 2
         ) {
             twoAlert("File size exceeds account capacity.");
-
             return;
         }
 
+        // If first time share with owner
         if (saveUsers[indexShareUser].filesShared[owner] === undefined) {
             saveUsers[indexShareUser].filesShared[owner] = {};
             saveUsers[indexShareUser].filesShared[owner][fileName] =
@@ -152,6 +145,7 @@ class Cloud {
                 saveUsers[indexUser].files[fileName];
         }
 
+        // if Basic Account take half of file size from memory
         if (saveUsers[indexShareUser].type === "Basic") {
             saveUsers[indexShareUser].memory -=
                 saveUsers[indexUser].files[fileName] / 2;
@@ -162,7 +156,6 @@ class Cloud {
     minspace() {
         if (saveUsers[0] === undefined) {
             twoAlert("No accounts.");
-
             return;
         }
 
@@ -183,7 +176,6 @@ class Cloud {
 
         if (userIndex === undefined) {
             twoAlert("Account does not exist.");
-
             return;
         }
 
@@ -245,38 +237,40 @@ class Cloud {
             findUserIndex(updateAccount) === undefined
         ) {
             twoAlert("Account does not exist.");
-
             return;
         }
 
+        //  If first account is not the owner of the file
         if (saveUsers[ownerIndex].files[file] === undefined) {
             twoAlert("File does not exist.");
-
             return;
         }
 
+        // If file does not exist
         if (
             saveUsers[ownerIndex].files[file] === undefined &&
             saveUsers[updateAccountIndex].files[file] === undefined
         ) {
             twoAlert("File not shared.");
-
             return;
         }
 
+        // If first account !== second account and file was not shared with the second account
         if (
             owner !== updateAccount &&
+            // If second account don't have any file file shared with first account
             saveUsers[updateAccountIndex].filesShared[owner] === undefined
         ) {
             twoAlert("File not shared.");
-
             return;
         } else if (
             owner !== updateAccount &&
+            // If second account already have file file shared with first account
             saveUsers[updateAccountIndex].filesShared[owner][file] === undefined
         ) {
         }
 
+        // Save the update
         if (saveUsers[ownerIndex].updates[file] === undefined) {
             saveUsers[ownerIndex].updates[file] = [updateAccount];
         } else {
@@ -292,24 +286,21 @@ class Cloud {
 
         if (indexAccount === undefined) {
             twoAlert("Account does not exist.");
-
             return;
         }
 
         if (saveUsers[indexAccount].files[file] === undefined) {
             twoAlert("File does not exist.");
-
             return;
         }
 
+        // If no updates return account that created the file
         if (saveUsers[indexAccount].updates[file] === undefined) {
             twoAlert(`Last update: ${account}`);
-
             return;
         }
 
-        alert(`Last update: ${saveUsers[indexAccount].updates[file][0]}`);
-        alert("");
+        twoAlert(`Last update: ${saveUsers[indexAccount].updates[file][0]}`);
     }
 }
 const eddisCloud = new Cloud();
@@ -331,7 +322,6 @@ const getCommand = () => {
     saveCommand[0] = saveCommand[0].toLowerCase();
     if (saveCommand[0] === "exit") {
         twoAlert("Exiting...");
-
         return false;
     }
 
